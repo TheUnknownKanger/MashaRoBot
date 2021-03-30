857bc0f9cc0622b54befaf57d23b8fd156f98043
The Unknown <77438385+TheUnknownKanger@users.noreply.github.com>
2021-03-30 15:43:39 +0530

    Update admin.py

--- MashaRoBot/modules/admin.py
+++ MashaRoBot/modules/admin.py
@@ -629,6 +629,7 @@ def adminlist(update, context):
 
 __help__ = """
  ❍ /admins*:* list of admins in the chat
+
 *Admins only:*
  ❍ /pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
  ❍ /unpin*:* unpins the currently pinned message
@@ -636,23 +637,13 @@ __help__ = """
  ❍ /promote*:* promotes the user
  ❍ /demote*:* demotes the user
  ❍ /title <title here>*:* sets a custom title for an admin that the bot promoted
- ❍ /setgtitle <newtitle>*:* Sets new chat title in your group.
- ❍ /setgpic*:* As a reply to file or photo to set group profile pic!
- ❍ /delgpic*:* Same as above but to remove group profile pic.
- ❍ /setsticker*:* As a reply to some sticker to set it as group sticker set!
- ❍ /setdescription <description>*:* Sets new chat description in group.
+ ❍ /setgtitle <newtitle>: Sets new chat title in your group.
+ ❍ /setgpic: As a reply to file or photo to set group profile pic!
+ ❍ /delgpic: Same as above but to remove group profile pic.
+ ❍ /setsticker: As a reply to some sticker to set it as group sticker set!
+ ❍ /setdescription: <description> Sets new chat description in group.
  ❍ /admincache*:* force refresh the admins list
  ❍ /antispam <on/off/yes/no>*:* Will toggle our antispam tech or return your current settings.
- ❍ /addnt*:* Adds Group to NightMode Chats
- ❍ /rmnt*:* Removes Group From NightMode Chats
- ❍ /addnsfw*:* Adds The Group to nsfw Watch List
- ❍ /rmnsfw*:* Removes The Group From nsfw Watch List
- ❍ /del*:* deletes the message you replied to
- ❍ /purge*:* deletes all messages between this and the replied to message.
- ❍ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
-*Note:* Night Mode chats get Automatically closed at 12 am(IST)
-and Automatically openned at 6 am(IST) To Prevent Night Spams.
-⚠️ `Read from top`
 """
 
 ADMINLIST_HANDLER = DisableAbleCommandHandler("admins", adminlist)
