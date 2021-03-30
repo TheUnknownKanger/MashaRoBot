1a51c4f93ee13c50f5fb278536b588227b06e660
The Unknown <77438385+TheUnknownKanger@users.noreply.github.com>
2021-03-30 15:06:40 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -86,6 +86,7 @@ buttons = [
     [
         InlineKeyboardButton(text="ℹ️ ABOUT", callback_data="masha_"),
         InlineKeyboardButton(text="📚 COMMANDS", callback_data="help_back"),
+        InlineKeyboardButton(text="🇮🇳 Alita Devs", url="t.me/AlitaDevs"), 
     ],
     [
         InlineKeyboardButton(
